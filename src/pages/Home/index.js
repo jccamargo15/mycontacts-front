6939da3d0d7a9b6bdfeb63f3e1dda041cbec49ce
José Carlos de Camargo <jccamargo15@gmail.com>
2023-12-07 17:59:54 -0300

    feat: criando a página Home e refatorando o Header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,20 @@
 import {
-  Container, Header, ListContainer, Card,
+  Container, InputSearchContainer, Header, ListContainer, Card,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
-export default function ContactsList() {
+export default function Home() {
   return (
     <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+
       <Header>
-        <string>3 contatos</string>
+        <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
 
