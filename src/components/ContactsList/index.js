48f2ebfad7440411954d7062b88eb13b8a18a0f5
José Carlos de Camargo <jccamargo15@gmail.com>
2023-12-06 23:17:17 -0300

    feat: criando o header do contact list

---
+++ src/components/ContactsList/index.js
@@ -0,0 +1,12 @@
+import { Container, Header } from './styles';
+
+export default function ContactsList() {
+  return (
+    <Container>
+      <Header>
+        <string>3 contatos</string>
+        <a href="/">Novo contato</a>
+      </Header>
+    </Container>
+  );
+}
