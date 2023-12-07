fb2b3216649cea7326f0cc08dc51d08a44de4c79
José Carlos de Camargo <jccamargo15@gmail.com>
2023-12-07 15:18:27 -0300

    feat: criando a listagem de contatos

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,6 +1,10 @@
-import { Container, Header, ListContainer } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -17,6 +21,72 @@ export default function ContactsList() {
             <img src={arrow} alt="Arrow" />
           </button>
         </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>José Camargo</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>email@gmail.com</span>
+            <span>(11) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>José Camargo</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>email@gmail.com</span>
+            <span>(11) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>José Camargo</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>email@gmail.com</span>
+            <span>(11) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
       </ListContainer>
     </Container>
   );
