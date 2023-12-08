f12fb2f448fbed9e32e6dba5aaf845027891f535
José Carlos de Camargo <jccamargo15@gmail.com>
2023-12-08 18:47:54 -0300

    feat: Criando os componentes Input e Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,18 @@
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 import PageHeader from '../../components/PageHeader';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo contato" />
+    <>
+      <PageHeader title="Novo contato" />
+
+      <Input type="text" placeholder="Nome" />
+
+      <Select>
+        <option value="0">Pessoa física</option>
+        <option value="1">Pessoa jurídica</option>
+      </Select>
+    </>
   );
 }
