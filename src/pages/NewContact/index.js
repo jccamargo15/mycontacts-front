68cd352c840c59ff5dc1fc8873542214d4f83dbe
José Carlos de Camargo <jccamargo15@gmail.com>
2023-12-08 23:38:15 -0300

    feat: criando o componente Button

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
+import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
-import PageHeader from '../../components/PageHeader';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -13,6 +14,13 @@ export default function NewContact() {
         <option value="0">Pessoa física</option>
         <option value="1">Pessoa jurídica</option>
       </Select>
+
+      <Button type="button">
+        Salvar alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
     </>
   );
 }
