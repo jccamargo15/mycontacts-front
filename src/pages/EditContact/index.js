3047e79507f7700ce14d9af9fec99cfc290eb4eb
José Carlos de Camargo <jccamargo15@gmail.com>
2023-12-09 21:26:24 -0300

    feat: criando o componente ContactForm

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,7 +1,12 @@
 import PageHeader from '../../components/PageHeader';
+import ContactForm from '../../components/ContactForm';
 
 export default function EditContact() {
   return (
-    <PageHeader title="Editar José Camargo" />
+    <>
+      <PageHeader title="Editar José Camargo" />
+
+      <ContactForm buttonLabel="Salvar alterações" />
+    </>
   );
 }
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,26 +1,12 @@
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
-import Input from '../../components/Input';
-import Select from '../../components/Select';
-import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo contato" />
 
-      <Input type="text" placeholder="Nome" />
-
-      <Select>
-        <option value="0">Pessoa física</option>
-        <option value="1">Pessoa jurídica</option>
-      </Select>
-
-      <Button type="button">
-        Salvar alterações
-      </Button>
-      <Button type="button" disabled>
-        Salvar alterações
-      </Button>
+      <ContactForm buttonLabel="Cadastrar" />
     </>
   );
 }
